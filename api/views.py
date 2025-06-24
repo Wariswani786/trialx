6c869b37f16209f7e84803cefcb9d4f4ca68a3bf
Wariswani786 <wkwarisback5@gmail.com>
2025-06-24 16:22:32 +0530

    Added user registration api with jwt

---
+++ api/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer  # ✅ Make sure file is named serializers.py
+
+class RegisterView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'username': user.username,
+                'email': user.email
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
